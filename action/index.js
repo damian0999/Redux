41085dc96db89a9a82848fa15bb6e088bd23fc50
Damian <labasdamian@gmail.com>
2016-10-04 22:16:57 +0200

    new structure of files

---
+++ action/index.js
@@ -0,0 +1,22 @@
+var decrement = function(){
+  return {
+    type: 'DECREMENT'
+  };
+}
+var increment = function(){
+  return{
+    type: 'INCREMENT'
+  };
+}
+var clear = function(){
+  return{
+    type: 'CLEAR'
+  };
+}
+var getSum = function(a, b){
+    return{
+      type: 'SUM',
+      a:a,
+      b:b
+    };
+}
