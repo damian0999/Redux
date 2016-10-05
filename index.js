eada76d7cbf615aad30bdcce100d5b96d65d8717
Damian <labasdamian@gmail.com>
2016-10-05 13:18:10 +0200

    add middleware

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
-var store = Redux.createStore(combineReducer);
+const logger = function(store){
+  return function(next){
+    return function(action){
+      console.log('currentState = ', store.getState());
+      console.info('action = ', action)
+      next(action);
+      console.log('currentState = ', store.getState());
+    }
+  }
+}
+const crashReporter = function(store){
+  return function(next) {
+    return function(action) {
+      try {
+        next(action);
+      } catch (e) {
+        console.error('error with action = ', action)
+        console.error(e);
+      }
+    }
+  }
+}
+
+var store = Redux.createStore(combineReducer, Redux.applyMiddleware(logger, crashReporter));
 function render(){
   var state = store.getState();
   document.getElementById('value').innerHTML = state.count;
--- reducer/sum.js
+++ reducer/sum.js
@@ -7,9 +7,14 @@ function sum(currentState, action){
   switch (action.type) {
     case 'SUM':
       nextState = parseInt(action.a) + parseInt(action.b);
+      funcWithError();
       return nextState;
     default:
       nextState = currentState;
       return nextState;
   }
+}
+
+function funcWithError(){
+  throw Error('an error from sum');
 }
